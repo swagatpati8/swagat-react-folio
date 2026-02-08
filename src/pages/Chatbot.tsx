2c2519890e4a9828147e1ed3e68a1e6d790c0a17
Swagat Pati <141627705+swagatpati8@users.noreply.github.com>
2026-02-07 21:12:22 -0500

    added to vercel ignore to fix vercel build issues

--- src/pages/Chatbot.tsx
+++ src/pages/Chatbot.tsx
@@ -23,6 +23,7 @@ const Chatbot = () => {
     
     try {
       const response = await fetch("http://127.0.0.1:5000/api/chat", {
+        
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({message: input}),
